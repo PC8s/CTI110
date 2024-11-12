1d0502ce4a4e8fe0460e5c71e13d7d33d82c55d0
PC8s <160653747+PC8s@users.noreply.github.com>
2024-11-12 16:33:46 +0000

    added value-returing function

--- function_examples1.py
+++ function_examples1.py
@@ -6,12 +6,33 @@ def my_animal(name, sound, pound_food):
     print(f"The {name} eats {pound_food} pound of food a day")
     print(f"The {name} eats {pound_food * 7} pound of food a week")
 
+def getName():
+    name = input("Enter your name: ")
+    return name + "******"
+
+def displayName(first):
+    lastName = input("Enter your name: ")
+    fullName = first + "" + lastName
+    return fullName
+
+
+
+
 # Create a main function - all logic goes here
 def main():
     print("The main function is executing!")
     print()
     # Call the my_animal function
     my_animal("Cat", "meow", 20.5)
+    print()
+    my_animal("Mouse", "ekekek", 0.2)
+
+    # Call the value-returning function
+    myName = getName()
+    print(myName)
+    print()
+    print(displayName(myName))
+
 
 # Call the main function
 if __name__ == "__main__":
